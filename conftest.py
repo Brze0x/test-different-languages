9f0ecb3c15364826f52f9e83c9778ba86a8d305b
Brze0x <58706694+Brze0x@users.noreply.github.com>
2022-08-12 15:07:06 +0300

    changed addoption

--- conftest.py
+++ conftest.py
@@ -6,7 +6,7 @@ def pytest_addoption(parser):
     parser.addoption('--browser_name', action='store', default="chrome",
                      help="Choose browser: chrome or firefox")
     parser.addoption('--language', action='store', default=None,
-                     help="Choose language es or en")
+                     help="Choose language")
 
 
 @pytest.fixture(scope="function")
