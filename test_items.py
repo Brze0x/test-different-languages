8c082cfc36e940ee050bf59d962abceb530c654f
Brze0x <58706694+Brze0x@users.noreply.github.com>
2022-08-12 13:22:24 +0300

    fixed test

--- test_items.py
+++ test_items.py
@@ -5,4 +5,4 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_check_add_button(browser):
     browser.get(link)
-    assert browser.find_elements(By.CSS_SELECTOR,".btn-add-to-basket"), "Add to basket button not found"
+    assert browser.find_elements(By.CSS_SELECTOR, ".btn-add-to-basket"), "Add to basket button not found"
